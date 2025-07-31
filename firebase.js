01de5a96a4a12f6721d158877c51a2cacdf1df85
Prakash Mohit <prakashmohit21x@gmail.com>
2025-08-01 04:07:55 +0530

    Updated firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
+const fs = require("fs");
+const path = require("path");
 const admin = require("firebase-admin");
 
-const firebaseConfig = JSON.parse(process.env.FIREBASE_KEY);
-
-// Fix the private key by replacing escaped newlines
-firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
+// Read JSON content from the file
+const serviceAccountPath = path.join(__dirname, "firebaseServiceAccountKey.json");
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
 module.exports = db;
-
